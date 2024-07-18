2ae54f46583ec6516ab61aa93ca995f31a51bbb1
iman barzegaran <133756389+Imanbarzegaran@users.noreply.github.com>
2024-07-18 14:03:04 +0330

    start project

---
+++ WeatherAnimate/WeatherAnimate/WeatherAnimateApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAnimateApp.swift
+//  WeatherAnimate
+//
+//  Created by MacBook Air on 7/18/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAnimateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
