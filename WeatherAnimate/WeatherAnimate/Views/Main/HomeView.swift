2f40235fc55c430a9a23ac4e89c6c3436420b631
iman barzegaran <133756389+Imanbarzegaran@users.noreply.github.com>
2024-07-20 18:32:34 +0330

    create tab bar  and add arc shape to tab bar

--- WeatherAnimate/WeatherAnimate/Views/Main/HomeView.swift
+++ WeatherAnimate/WeatherAnimate/Views/Main/HomeView.swift
@@ -9,42 +9,66 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        ZStack {
-            
-            // MARK: Background Color
-            Color.background
-                .ignoresSafeArea()
-            
-            // MARK: Background Image
-            Image("Background")
-                .resizable()
-                .ignoresSafeArea()
-            
-            // MARK: House Image
-            Image("House")
-                .frame(maxHeight: .infinity, alignment: .top)
-                .padding(.top, 257)
-            
-            VStack {
-                Text("Montreal")
-                    .font(.largeTitle)
+        NavigationView {
+          
+            ZStack {
                 
-                VStack {
-                    Text("19°" + "\n" + "Mostly Clear")
-                    Text("H:24°    L:18°")
-                        .font(.title3.weight(.semibold))
+                // MARK: Background Color
+                Color.background
+                    .ignoresSafeArea()
+                
+                // MARK: Background Image
+                Image("Background")
+                    .resizable()
+                    .ignoresSafeArea()
+                
+                // MARK: House Image
+                Image("House")
+                    .frame(maxHeight: .infinity, alignment: .top)
+                    .padding(.top, 257)
+                
+                // MARK: Info from location and weather condition text
+
+                VStack(spacing: -10) {
+                    Text("Montreal")
+                        .font(.largeTitle)
+                    
+                    VStack {
+                        Text(attributedString)
+                        Text("H:24°    L:18°")
+                            .font(.title3.weight(.semibold))
+                    }
+                    Spacer()
                 }
+                .padding(.top, 51)
                 
-                Spacer()
+                // MARK: Tab Bar
+                TabBar(action: {})
             }
-            .padding()
-
-            
+            .navigationBarHidden(true)
         }
-
+    }
+    
+    private var attributedString: AttributedString {
+        var string = AttributedString("19°" + "\n " + "Mostly Clear")
+        if let temp = string.range(of: "19°") {
+            string[temp].font = .system(size: 96, weight: .thin)
+            string[temp].foregroundColor = .primary
+        }
+        if let pipe = string.range(of: " | ") {
+            string[pipe].font = .title3.weight(.semibold)
+            string[pipe].foregroundColor = .secondary
+        }
+        
+        if let weather = string.range(of: "Mostly Clear") {
+            string[weather].font = .title3.weight(.semibold)
+            string[weather].foregroundColor = .secondary
+        }
+        return string
     }
 }
 
 #Preview {
     HomeView()
+        .preferredColorScheme(.dark)
 }
